b58cadeff88e18b648160e5f5aa7bbd94c56b5a8
Глеб Выгорчук <vygorcukgleb@gmail.com>
2025-01-31 12:54:01 +0500

    test

--- react/src/components/sectionAboutUs/SectionAboutUs.jsx
+++ react/src/components/sectionAboutUs/SectionAboutUs.jsx
@@ -1,4 +1,8 @@
 import SectionTitle from "../../assets/sectionTitle/SectionTitle"
+import breakfastImg from "../../assets/breakfast.png"
+import playgroundImg from "../../assets/playground.png"
+import filmsImg from "../../assets/films.png"
+import fusionImg from "../../assets/fusion.png"
 
 export default function SectionAboutUs({className, id}) {
     return (
@@ -11,28 +15,28 @@ export default function SectionAboutUs({className, id}) {
                         <p style={{fontWeight: '500'}}>Завтраки</p>
                         <p className="features__item_explain">Ежедневно<br /> до 15.00</p>
                     </div>
-                    <img src="./src/assets/breakfastjpg.jpg" alt="" />
+                    <img src={breakfastImg} alt="" />
                 </li>
                 <li className="features__item">
                     <div className="features__item_desc">
                         <p style={{fontWeight: '500'}}>Детская комната с няней</p>
                         <p className="features__item_explain">Чтобы всем было<br /> уютно и спокойно</p>
                     </div>
-                    <img src="./src/assets/playground.png" alt="" />
+                    <img src={playgroundImg} alt="" />
                 </li>
                 <li className="features__item">
                     <div className="features__item_desc">
                         <p style={{fontWeight: '500'}}>Киновечера</p>
                         <p className="features__item_explain">Даже дома не такая<br /> магическая атмосфера</p>
                     </div>
-                    <img src="./src/assets/films.png" alt="" />
+                    <img src={filmsImg} alt="" />
                 </li>
                 <li className="features__item">
                     <div className="features__item_desc">
                         <p style={{fontWeight: '500'}}>Фьюжн меню</p>
                         <p className="features__item_explain">Авторская кухня, где<br /> каждое блюдо уникально</p>
                     </div>
-                    <img src="./src/assets/fusion.png" alt="" />
+                    <img src={fusionImg} alt="" />
                 </li>
             </ul>
         </section>
