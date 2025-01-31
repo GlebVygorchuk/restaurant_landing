dc4f07e99e05038f3ac3431aeeb75c8f6b9875ad
Глеб Выгорчук <vygorcukgleb@gmail.com>
2025-01-31 13:33:16 +0500

    fixed image bug

--- react/src/components/sectionInterior/SectionInterior.jsx
+++ react/src/components/sectionInterior/SectionInterior.jsx
@@ -1,4 +1,15 @@
 import SectionTitle from "../../assets/sectionTitle/SectionTitle"
+import interiorFusion from "../../assets/interiorPics/fusion2.png"
+import interiorCheeseria from "../../assets/interiorPics/sirovarnya 1.png"
+import interiorCinemaEvening from "../../assets/interiorPics/kinovecher.png"
+import interiorDinner from "../../assets/interiorPics/dinner 1.png"
+import interiorPasta from "../../assets/interiorPics/pasta 1.png"
+import interiorRestaurant from "../../assets/interiorPics/restik 1.png"
+import interiorFamilyDinner from "../../assets/interiorPics/dinnerfamily 1.png"
+import interiorEggs from "../../assets/interiorPics/egs 1.png"
+import interiorWindow from "../../assets/interiorPics/window 1.png"
+import interiorWindowRest from "../../assets/interiorPics/windowrest 1.png"
+import interiorCinema from "../../assets/interiorPics/kino 1.png"
 
 export default function SectionInterior({className, id}) {
     return (
@@ -7,21 +18,21 @@ export default function SectionInterior({className, id}) {
 
             <div className="pictures">
                 <div className="pictures__section">
-                    <img src="./src/assets/interiorPics/fusion2.png" alt="" />
-                    <img src="./src/assets/interiorPics/sirovarnya 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/kinovecher.png" alt="" />
-                    <img src="./src/assets/interiorPics/dinner 1.png" alt="" />
+                    <img src={interiorFusion} alt="" />
+                    <img src={interiorCheeseria} alt="" />
+                    <img src={interiorCinemaEvening} alt="" />
+                    <img src={interiorDinner} alt="" />
                 </div>
                 <div className="pictures__section">
-                    <img src="./src/assets/interiorPics/pasta 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/restik 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/dinnerfamily 1.png" alt="" />
+                    <img src={interiorPasta} alt="" />
+                    <img src={interiorRestaurant} alt="" />
+                    <img src={interiorFamilyDinner} alt="" />
                 </div>
                 <div className="pictures__section">
-                    <img src="./src/assets/interiorPics/egs 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/window 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/windowrest 1.png" alt="" />
-                    <img src="./src/assets/interiorPics/kino 1.png" alt="" />
+                    <img src={interiorEggs} alt="" />
+                    <img src={interiorWindow} alt="" />
+                    <img src={interiorWindowRest} alt="" />
+                    <img src={interiorCinema} alt="" />
                 </div>
             </div>
         </section>
--- react/src/components/sectionMenu/SectionMenu.jsx
+++ react/src/components/sectionMenu/SectionMenu.jsx
@@ -1,4 +1,9 @@
 import SectionTitle from "../../assets/sectionTitle/SectionTitle"
+import breakfastBtn from "../../assets/menuButtons/ZAVTRAH.png"
+import dinnerBtn from "../../assets/menuButtons/DINNER 1.png"
+import barBtn from "../../assets/menuButtons/BAR 1.png"
+import chidlrenBtn from "../../assets/menuButtons/DETIEDA 1.png"
+import grillBtn from "../../assets/menuButtons/GRILL 1.png"
 import { AppContext } from "../../AppContext"
 import { useContext } from "react"
 
@@ -14,27 +19,27 @@ export default function SectionMenu({className, id}) {
             <div className="chapters">
                 <div className="chapters__header">
                     <div onClick={() => setMenuChapter('menu-breakfast')} className="chapters__item">
-                       <img src="./src/assets/menuButtons/ZAVTRAH.png" alt="" />
+                       <img src={breakfastBtn} alt="" />
                        <p>Завтраки</p>
                     </div>
                    <div onClick={() => setMenuChapter('menu-dinner')} className="chapters__item">
-                       <img src="./src/assets/menuButtons/DINNER 1.png" alt="" />
+                       <img src={dinnerBtn} alt="" />
                        <p>Основное</p>
                    </div>
                    <div onClick={() => setMenuChapter('menu-bar')} className="chapters__item">
-                       <img src="./src/assets/menuButtons/BAR 1.png" alt="" />
+                       <img src={barBtn} alt="" />
                        <p>Барное</p>
                    </div>
                 </div>
                 
                 <div className="chapters__footer">
                    <div onClick={() => setMenuChapter('menu-children')} className="chapters__item">
-                       <img src="./src/assets/menuButtons/DETIEDA 1.png" alt="" />
+                       <img src={chidlrenBtn} alt="" />
                        <p>Детское</p>
                    </div>
                    <div onClick={() => setMenuChapter('menu-grill')} className="chapters__item">
-                       <img src="./src/assets/menuButtons/GRILL 1.png" alt="" />
-                       <p>Гриль-меню</p>
+                       <img src={grillBtn} alt="" />
+                       <p>Гриль меню</p>
                    </div>
                 </div>
             </div>
